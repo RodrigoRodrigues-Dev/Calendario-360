043957ef2ca3c296526503f1721097ed94a7625b
Rodrigo Roigues <rs9844660@gmail.com>
2025-03-25 17:28:59 -0300

    feat: implement GlobalStyles and update color palette - Added GlobalStyles to centralize app styling for consistency across components - Defined and integrated a unified color scheme for improved UI coherence

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const Reset = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "Inter", sans-serif;
+  }
+
+  body {
+    font-size: 14px;
+  }
+`;
---
+++ src/styles/colors.ts
@@ -0,0 +1,9 @@
+const colors = {
+  primary: '#121212',
+  black: '#000',
+  grayishCyan: '#626363',
+  white: '#fff',
+  whiteEdgar: '#ededed'
+};
+
+export default colors;

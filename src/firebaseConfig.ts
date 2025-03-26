614884ee339f5b621f0605384f60f4cf2f1b61cc
Rodrigo Roigues <rs9844660@gmail.com>
2025-03-26 18:26:40 -0300

    feat: add firebaseConfig.ts configuration and update .gitignore to include .env

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: 'calendario-360-822e8.firebaseapp.com',
+  projectId: 'calendario-360-822e8',
+  storageBucket: 'calendario-360-822e8.firebasestorage.app',
+  messagingSenderId: '940931101140',
+  appId: '1:940931101140:web:293aee5b6ede6f8e28e641'
+};
+
+export default firebaseConfig;

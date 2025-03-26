1f52f806d17473dbe11991eb54216ef20e5d94d3
Rodrigo Roigues <rs9844660@gmail.com>
2025-03-26 19:38:05 -0300

    feat: implement Firebase authentication logic in LoginForm - Added email and password input fields with validation - Integrated Firebase Authentication for user sign-in - Stored user session data in Firebase Database upon successful login

--- src/components/form/LoginForm/index.tsx
+++ src/components/form/LoginForm/index.tsx
@@ -1,15 +1,82 @@
-import { AuthLandingLogin } from './styles';
+import { useState } from 'react';
+
+import { initializeApp } from 'firebase/app';
+import firebaseConfig from '../../../firebaseConfig';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { getDatabase, ref as dbRef, set } from 'firebase/database';
+
+import { AuthLandingLogin, AuthLandingLoginHeader } from './styles';
 import AuthLandingPanelInput from '../../auth/AuthLandingPanelInput';
 import AuthLandingPanelButtonForm from '../../auth/AuthLandingPanelButtonForm';
+import InvalidField from '../InvalidField';
+
+const firebaseApp = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseApp);
+const db = getDatabase(firebaseApp);
 
 const LoginForm = () => {
+  const [email, setEmail] = useState<string>('');
+  const [emailValid, setEmailValid] = useState<boolean>(true);
+
+  const [password, setPassword] = useState<string>('');
+  const [passwordValid, setPasswordValid] = useState<boolean>(true);
+
+  const handleEmailChange = (value: string) => {
+    setEmail(value);
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const valid = emailRegex.test(value);
+    setEmailValid(valid);
+    return valid;
+  };
+
+  const handlePasswordChange = (value: string) => {
+    setPassword(value);
+    const meetsLength = value.trim().length >= 7;
+    const letterMatches = value.match(/[A-Za-z]/g);
+    const letterCount = letterMatches ? letterMatches.length : 0;
+    const meetsLetterCount = letterCount <= 2;
+    setPasswordValid(meetsLength && meetsLetterCount);
+  };
+
+  const signIn = () => {
+    if (!emailValid || !passwordValid) {
+      return;
+    }
+
+    signInWithEmailAndPassword(auth, email, password).then(userCredential => {
+      console.log('Login realizado com sucesso');
+      const userId = userCredential.user.uid;
+      set(dbRef(db, 'users/' + userId), {
+        email
+      });
+    });
+  };
+
   return (
     <AuthLandingLogin>
-      <h2>Fazer Log-In</h2>
-      <p>É um prazer ter você aqui novamente!</p>
-      <AuthLandingPanelInput type="text" placeholder="Email" />
-      <AuthLandingPanelInput type="password" placeholder="Senha" />
-      <AuthLandingPanelButtonForm>Fazer Log-In</AuthLandingPanelButtonForm>
+      <AuthLandingLoginHeader>
+        <h2>Fazer Log-In</h2>
+        <p>É um prazer ter você aqui novamente!</p>
+      </AuthLandingLoginHeader>
+      {!emailValid && <InvalidField>Email Invalido</InvalidField>}
+      <AuthLandingPanelInput
+        type="text"
+        placeholder="Email"
+        onChange={e => handleEmailChange(e.target.value)}
+      />
+      {!passwordValid && (
+        <InvalidField>
+          A senha deve ter 7 ou mais caracteres e no máximo 2 letras
+        </InvalidField>
+      )}
+      <AuthLandingPanelInput
+        type="password"
+        placeholder="Senha"
+        onChange={e => handlePasswordChange(e.target.value)}
+      />
+      <AuthLandingPanelButtonForm onClick={signIn}>
+        Fazer Log-In
+      </AuthLandingPanelButtonForm>
     </AuthLandingLogin>
   );
 };
--- src/components/form/LoginForm/styles.ts
+++ src/components/form/LoginForm/styles.ts
@@ -2,5 +2,9 @@ import styled from 'styled-components';
 import { FormContainerStyled } from '../FormContainer/styled';
 
 export const AuthLandingLogin = styled(FormContainerStyled)`
-  gap: 2rem;
+  gap: 2.5rem;
+`;
+
+export const AuthLandingLoginHeader = styled(FormContainerStyled)`
+  gap: 1rem;
 `;

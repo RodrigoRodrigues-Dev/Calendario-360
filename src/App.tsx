63498ca32fc4b2c57ffdebaefff7269a77699dd7
Rodrigo Roigues <rs9844660@gmail.com>
2025-04-08 19:55:11 -0300

    feat: setup App layout with Redux and PersistGate
    - Wrapped main App component with Redux Provider and PersistGate for state persistence.
    - Integrated Header, Outlet for nested routes, and NavBar for a consistent layout.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,21 @@
-function App() {}
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Outlet } from 'react-router-dom';
+
+import Header from './containers/Header';
+import NavBar from './containers/NavBar';
+import { store, persistor } from './store';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Header />
+        <Outlet />
+        <NavBar />
+      </PersistGate>
+    </Provider>
+  );
+}
 
 export default App;
